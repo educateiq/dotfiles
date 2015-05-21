13f9eee70bdb5db42c1ddc55d39d65c938d2ca5e
Ubuntu <ubuntu@ip-172-31-39-233.us-west-2.compute.internal>
2015-05-21 03:35:49 +0000

    add package manager. add js2-mode ac-js2

---
+++ .emacs.d/package-manager.el
@@ -0,0 +1,12 @@
+;;; Emacs is not a package manager, and here we load its package manager!
+(require 'package)
+(dolist (source '(("marmalade" . "http://marmalade-repo.org/packages/")
+                  ("elpa" . "http://tromey.com/elpa/")
+                  ;; TODO: Maybe, use this after emacs24 is released
+                  ;; (development versions of packages)
+                  ("melpa" . "http://melpa.milkbox.net/packages/")
+                  ))
+  (add-to-list 'package-archives source t))
+(package-initialize)
+
+;;to install press M-x list-packages and hit return on the package to install
